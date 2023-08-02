8be0e1db16caedd04175cd8da1c7def79e0d8a3f
AleZaldua <alejandrozaldua85@gmail.com>
2023-08-02 00:02:49 -0400

    last changes august 1

---
+++ Challenge 19/js/index.js
@@ -0,0 +1,13 @@
+function fade() {
+  $('.preloader').fadeOut("slow");  
+}
+setTimeout(fade, 3000);
+
+$(document).ready(function(){
+    $(".nav-link").mouseenter(function(){
+      $(".nav-link").css("background-color", "yellow");
+    });
+    $(".nav-link").mouseleave(function(){
+        $(".nav-link").css("background-color", "white");
+      });
+});
