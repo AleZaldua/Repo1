38a35272e6b7b9ae8d3946bc9ad35c03862ba9fe
AleZaldua <alejandrozaldua85@gmail.com>
2023-08-03 22:18:02 -0400

    changes August 3

--- Challenge 19/js/index.js
+++ Challenge 19/js/index.js
@@ -12,4 +12,27 @@ $(document).ready(function(){
       });
 });
 
+$("#example").typer({
+  strings: [
+    "Concept",
+    "Stories",
+    "Projects"
+  ]
+});
+
+$("#example").typer({
+  typeSpeed: 60,
+  backspaceSpeed: 20,
+  backspaceDelay: 800,
+  repeatDelay: 1000,
+  repeat: true,
+  autoStart: true,
+  startDelay: 100,
+});
+
+$("#example").typer({
+  useCursor: true
+});
+
+
 
